5de459854b664879c6fdd98a14548ec5fc307146
Mohit Sudhakar <ms5sw@virginia.edu>
2020-09-14 12:01:28 -0400

    changes

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class CONFIG(object):
         self.dataset = 'R8'
         self.model = 'gcn'  # 'gcn', 'gcn_cheby', 'dense'
         self.learning_rate = 0.02   # Initial learning rate.
-        self.epochs  = 20  # Number of epochs to train.
+        self.epochs  = 100  # Number of epochs to train.
         self.hidden1 = 200  # Number of units in hidden layer 1.
         self.dropout = 0.5  # Dropout rate (1 - keep probability).
         self.weight_decay = 0.   # Weight for L2 loss on embedding matrix.
--- train.py
+++ train.py
@@ -43,7 +43,7 @@ torch.manual_seed(seed)
 
 # Load data
 adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask, train_size, test_size = load_corpus(
-    cfg.dataset)
+	cfg.dataset)
 
 features = sp.identity(features.shape[0])  # featureless
 
@@ -51,19 +51,19 @@ features = sp.identity(features.shape[0])  # featureless
 # Some preprocessing
 features = preprocess_features(features)
 if cfg.model == 'gcn':
-    support = [preprocess_adj(adj)]
-    num_supports = 1
-    model_func = GCN
+	support = [preprocess_adj(adj)]
+	num_supports = 1
+	model_func = GCN
 elif cfg.model == 'gcn_cheby':
-    support = chebyshev_polynomials(adj, cfg.max_degree)
-    num_supports = 1 + cfg.max_degree
-    model_func = GCN
+	support = chebyshev_polynomials(adj, cfg.max_degree)
+	num_supports = 1 + cfg.max_degree
+	model_func = GCN
 elif cfg.model == 'dense':
-    support = [preprocess_adj(adj)]  # Not used
-    num_supports = 1
-    model_func = MLP
+	support = [preprocess_adj(adj)]  # Not used
+	num_supports = 1
+	model_func = MLP
 else:
-    raise ValueError('Invalid argument for model: ' + str(cfg.model))
+	raise ValueError('Invalid argument for model: ' + str(cfg.model))
 
 
 # Define placeholders
@@ -76,7 +76,7 @@ tm_train_mask = torch.transpose(torch.unsqueeze(t_train_mask, 0), 1, 0).repeat(1
 
 t_support = []
 for i in range(len(support)):
-    t_support.append(torch.Tensor(support[i]))
+	t_support.append(torch.Tensor(support[i]))
 
 # if torch.cuda.is_available():
 #     model_func = model_func.cuda()
@@ -88,7 +88,7 @@ for i in range(len(support)):
 #     tm_train_mask = tm_train_mask.cuda()
 #     for i in range(len(support)):
 #         t_support = [t.cuda() for t in t_support if True]
-        
+
 model = model_func(input_dim=features.shape[0], support=t_support, num_classes=y_train.shape[1])
 
 
@@ -99,20 +99,20 @@ optimizer = torch.optim.Adam(model.parameters(), lr=cfg.learning_rate)
 
 # Define model evaluation function
 def evaluate(features, labels, mask):
-    t_test = time.time()
-    # feed_dict_val = construct_feed_dict(
-    #     features, support, labels, mask, placeholders)
-    # outs_val = sess.run([model.loss, model.accuracy, model.pred, model.labels], feed_dict=feed_dict_val)
-    model.eval()
-    with torch.no_grad():
-        logits = model(features)
-        t_mask = torch.from_numpy(np.array(mask*1., dtype=np.float32))
-        tm_mask = torch.transpose(torch.unsqueeze(t_mask, 0), 1, 0).repeat(1, labels.shape[1])
-        loss = criterion(logits * tm_mask, torch.max(labels, 1)[1])
-        pred = torch.max(logits, 1)[1]
-        acc = ((pred == torch.max(labels, 1)[1]).float() * t_mask).sum().item() / t_mask.sum().item()
-        
-    return loss.numpy(), acc, pred.numpy(), labels.numpy(), (time.time() - t_test)
+	t_test = time.time()
+	# feed_dict_val = construct_feed_dict(
+	#     features, support, labels, mask, placeholders)
+	# outs_val = sess.run([model.loss, model.accuracy, model.pred, model.labels], feed_dict=feed_dict_val)
+	model.eval()
+	with torch.no_grad():
+		logits = model(features)
+		t_mask = torch.from_numpy(np.array(mask*1., dtype=np.float32))
+		tm_mask = torch.transpose(torch.unsqueeze(t_mask, 0), 1, 0).repeat(1, labels.shape[1])
+		loss = criterion(logits * tm_mask, torch.max(labels, 1)[1])
+		pred = torch.max(logits, 1)[1]
+		acc = ((pred == torch.max(labels, 1)[1]).float() * t_mask).sum().item() / t_mask.sum().item()
+
+	return loss.numpy(), acc, pred.numpy(), labels.numpy(), (time.time() - t_test)
 
 
 
@@ -121,28 +121,28 @@ val_losses = []
 # Train model
 for epoch in range(cfg.epochs):
 
-    t = time.time()
-    
-    # Forward pass
-    logits = model(t_features)
-    loss = criterion(logits * tm_train_mask, torch.max(t_y_train, 1)[1])    
-    acc = ((torch.max(logits, 1)[1] == torch.max(t_y_train, 1)[1]).float() * t_train_mask).sum().item() / t_train_mask.sum().item()
-        
-    # Backward and optimize
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+	t = time.time()
 
-    # Validation
-    val_loss, val_acc, pred, labels, duration = evaluate(t_features, t_y_val, val_mask)
-    val_losses.append(val_loss)
+	# Forward pass
+	logits = model(t_features)
+	loss = criterion(logits * tm_train_mask, torch.max(t_y_train, 1)[1])
+	acc = ((torch.max(logits, 1)[1] == torch.max(t_y_train, 1)[1]).float() * t_train_mask).sum().item() / t_train_mask.sum().item()
 
-    print_log("Epoch: {:.0f}, train_loss= {:.5f}, train_acc= {:.5f}, val_loss= {:.5f}, val_acc= {:.5f}, time= {:.5f}"\
-                .format(epoch + 1, loss, acc, val_loss, val_acc, time.time() - t))
+	# Backward and optimize
+	optimizer.zero_grad()
+	loss.backward()
+	optimizer.step()
 
-    if epoch > cfg.early_stopping and val_losses[-1] > np.mean(val_losses[-(cfg.early_stopping+1):-1]):
-        print_log("Early stopping...")
-        break
+	# Validation
+	val_loss, val_acc, pred, labels, duration = evaluate(t_features, t_y_val, val_mask)
+	val_losses.append(val_loss)
+
+	print_log("Epoch: {:.0f}, train_loss= {:.5f}, train_acc= {:.5f}, val_loss= {:.5f}, val_acc= {:.5f}, time= {:.5f}"\
+				.format(epoch + 1, loss, acc, val_loss, val_acc, time.time() - t))
+
+	if epoch > cfg.early_stopping and val_losses[-1] > np.mean(val_losses[-(cfg.early_stopping+1):-1]):
+		print_log("Early stopping...")
+		break
 
 
 print_log("Optimization Finished!")
@@ -155,9 +155,9 @@ print_log("Test set results: \n\t loss= {:.5f}, accuracy= {:.5f}, time= {:.5f}".
 test_pred = []
 test_labels = []
 for i in range(len(test_mask)):
-    if test_mask[i]:
-        test_pred.append(pred[i])
-        test_labels.append(np.argmax(labels[i]))
+	if test_mask[i]:
+		test_pred.append(pred[i])
+		test_labels.append(np.argmax(labels[i]))
 
 
 print_log("Test Precision, Recall and F1-Score...")
@@ -181,39 +181,39 @@ print_log('\rWord_embeddings:')
 print(word_embeddings)
 
 with open('./data/corpus/' + dataset + '_vocab.txt', 'r') as f:
-    words = f.readlines()
+	words = f.readlines()
 
 vocab_size = len(words)
 word_vectors = []
 for i in range(vocab_size):
-    word = words[i].strip()
-    word_vector = word_embeddings[i]
-    word_vector_str = ' '.join([str(x) for x in word_vector])
-    word_vectors.append(word + ' ' + word_vector_str)
+	word = words[i].strip()
+	word_vector = word_embeddings[i]
+	word_vector_str = ' '.join([str(x) for x in word_vector])
+	word_vectors.append(word + ' ' + word_vector_str)
 
 word_embeddings_str = '\n'.join(word_vectors)
 with open('./data/' + dataset + '_word_vectors.txt', 'w') as f:
-    f.write(word_embeddings_str)
+	f.write(word_embeddings_str)
 
 
 
 doc_vectors = []
 doc_id = 0
 for i in range(train_size):
-    doc_vector = train_doc_embeddings[i]
-    doc_vector_str = ' '.join([str(x) for x in doc_vector])
-    doc_vectors.append('doc_' + str(doc_id) + ' ' + doc_vector_str)
-    doc_id += 1
+	doc_vector = train_doc_embeddings[i]
+	doc_vector_str = ' '.join([str(x) for x in doc_vector])
+	doc_vectors.append('doc_' + str(doc_id) + ' ' + doc_vector_str)
+	doc_id += 1
 
 for i in range(test_size):
-    doc_vector = test_doc_embeddings[i]
-    doc_vector_str = ' '.join([str(x) for x in doc_vector])
-    doc_vectors.append('doc_' + str(doc_id) + ' ' + doc_vector_str)
-    doc_id += 1
+	doc_vector = test_doc_embeddings[i]
+	doc_vector_str = ' '.join([str(x) for x in doc_vector])
+	doc_vectors.append('doc_' + str(doc_id) + ' ' + doc_vector_str)
+	doc_id += 1
 
 doc_embeddings_str = '\n'.join(doc_vectors)
 with open('./data/' + dataset + '_doc_vectors.txt', 'w') as f:
-    f.write(doc_embeddings_str)
+	f.write(doc_embeddings_str)
 
 
 
